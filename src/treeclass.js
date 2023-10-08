9f89a7746a435a5d1e7f6ce6ccdcd17641b902d8
BuonicosII <118397101+BuonicosII@users.noreply.github.com>
2023-10-08 19:35:46 +0200

    Start building remove node function

--- src/treeclass.js
+++ src/treeclass.js
@@ -26,9 +26,54 @@ class Tree {
         }
 
         findSmallestNode(this.root);
-    }
+    };
 
-    
-};
+    remove(value) {
+        
+        function findNodeToRemove(node) {
+           
+            //base case: if either node.left or node.right are leaf nodes and none equals the value, return. The value has not been found.
+            if (node.left.left === null && 
+                node.left.right === null && 
+                node.right.left === null && 
+                node.right.right === null &&
+                node.left.value !== value && 
+                node.right.value !== value) {
+                    return;
+            //if node.left equals the value
+            } else if (node.left.value === value) {
+                //if it's a leaf node
+                if (node.left.left === null && node.left.right === null) {
+                    node.left === null;
+                    return;
+                //if node left has a left child
+                } else if (node.left.left !== null && node.left.right === null) {
+                    node.left === node.left.left;
+                    return;
+                //if node left has a right child
+                } else if (node.left.left === null && node.left.right !== null){
+                    node.left = node.left.right;
+                    return; 
+                }
+            //if node.right equals the value
+            } else if (node.right === value) {
+                //if it's a leaf node
+                if (node.right.left === null && node.right.right === null) {
+                    node.right = null;
+                    return;
+                //if node right has a left child
+                } else if (node.right.left !== null && node.right.right === null) {
+                    node.right === node.right.left;
+                    return;
+                //if node right has a right child
+                } else if (node.right.left === null && node.right.right !== null){
+                    node.right = node.right.right;
+                    return; 
+                }
+            }
+            }
+            findNodeToRemove(this.root)
+        }
+    };
 
 export { Tree }
